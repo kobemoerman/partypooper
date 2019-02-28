bf79c2cb571f1af11985851474da207e0a1cdb38
kobemoerman <38468122+kobemoerman@users.noreply.github.com>
2019-02-28 18:53:04 +0100

    bottom navigation

--- app/src/main/java/com/application/android/partypooper/HomeActivity.java
+++ app/src/main/java/com/application/android/partypooper/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.application.android.partypooper;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,42 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        navigationButtonListener();
+
+        updateFragment(new HomeFragment());
+
+    }
+
+    private void navigationButtonListener() {
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_friends:
+                        selectedFragment = new FriendsFragment();
+                        break;
+                    case R.id.nav_calendar:
+                        selectedFragment = new EventsFragment();
+                        break;
+                    case R.id.nav_menu:
+                        selectedFragment = new MenuFragment();
+                        break;
+                }
+                updateFragment(selectedFragment);
+
+                return true;
+            }
+        });
+    }
+
+    private void updateFragment (Fragment selected) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragement_container, selected).commit();
     }
 }
---
+++ app/src/main/java/com/application/android/partypooper/MenuFragment.java
@@ -0,0 +1,21 @@
+package com.application.android.partypooper;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MenuFragment extends Fragment {
+
+    private Button loginButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_menu, container, false);
+    }
+}
